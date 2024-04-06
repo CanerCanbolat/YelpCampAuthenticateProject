f823c7c6266127ca03baf0d56913e400c0965768
Caner Canbolat <107541646+CanerCanbolat@users.noreply.github.com>
2024-04-06 23:56:55 +0300

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const descriptors = require("./descriptors");
+const places = require("./places");
+const Campground = require("../models/campground");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("Database connected");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "6610e4f5c272c8ecfd677ba2",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/random?camping",
+      description: "Lorem ipsum dolur sit amet.......",
+      price,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
